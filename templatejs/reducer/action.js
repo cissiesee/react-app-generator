c9f30635c03d387504d623164bb417b3a2ed0b2e
huangzt <huangzt@linkstec.com>
2018-01-26 14:20:15 +0800

    version 1.5.6

--- templatejs/reducer/action.js
+++ templatejs/reducer/action.js
@@ -1,4 +1,4 @@
-import * as actionTypes from "./<%=reducerName%>Type";
+import * as actionTypes from "./<%=reducerName%>Types";
 // import { /*todo*/ } from "../../service/<%=reducerName%>Service";
 
 /**
--- templatejs/reducer/types.js
+++ templatejs/reducer/types.js
@@ -1,1 +1,1 @@
-export const /*action type variable*/ = "<%=reducerName%>/*action name*/";
+export const <%=reducerName.toUpperCase() %>_/*type(UPPERCASE)*/ = "<%=reducerName%>/*action name*/";
